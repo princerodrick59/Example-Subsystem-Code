8930e81952154a64e42c7484944c80ffd0285fda
Prince <princerodrick59@gmail.com>
2025-11-17 22:45:02 -0600

    Remove unused TalonFX_Rollers_WithoutSensor import from intake commands

--- src/main/java/frc/robot/commands/Intake_Commands/Intake_WithSensor/Roller_Intake_WithSensor_CMD.java
+++ src/main/java/frc/robot/commands/Intake_Commands/Intake_WithSensor/Roller_Intake_WithSensor_CMD.java
@@ -6,7 +6,6 @@ package frc.robot.commands.Intake_Commands.Intake_WithSensor;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake.TalonFX_Rollers_WithSensor;
-import frc.robot.subsystems.Intake.TalonFX_Rollers_WithoutSensor;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class Roller_Intake_WithSensor_CMD extends Command {
