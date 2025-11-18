a1563a2ab3a5981546b80588a1b6e1d495813a4f
Prince <princerodrick59@gmail.com>
2025-11-17 21:38:47 -0600

    Added vendor dependencies and finished roller class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,20 +5,15 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.TalonFX_Rollers;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
+
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final TalonFX_Rollers m_exampleSubsystem = new TalonFX_Rollers();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -30,19 +25,9 @@ public class RobotContainer {
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+    
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/TalonFX_Rollers.java
@@ -0,0 +1,123 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import org.littletonrobotics.junction.AutoLogOutput;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TalonFX_Rollers extends SubsystemBase {
+  
+  private TalonFX rollerMotor;
+  private TalonFXConfiguration rollerConfig;
+
+
+  /**
+   * Creates a new TalonFX_Rollers subsystem.
+   * This involves the basic code for running the rollers of a motor.
+   * This subsystem does not involve the implementation of any sensor to stop the rollers.
+   * You will need to adjust the motor ID, inversion, neutral mode, and current limits based on your specific mechanism and requirements.
+   * This subsystem includes methods to intake, outtake, and stop the rollers.
+   * 
+   */
+  public TalonFX_Rollers() {
+
+    rollerMotor = new TalonFX(10); // Replace 10 with the actual device ID
+
+    rollerConfig = new TalonFXConfiguration()
+                        .withMotorOutput(new MotorOutputConfigs()
+                                              .withInverted(InvertedValue.CounterClockwise_Positive) //Set motor inversion based on mechanism
+                                              .withNeutralMode(NeutralModeValue.Brake))
+                        .withCurrentLimits(new CurrentLimitsConfigs()
+                                              .withSupplyCurrentLimit(15)); //Set current limit based on mechanism requirements
+
+    rollerMotor.getConfigurator().apply(rollerConfig);
+
+  }
+
+  /**
+   * Method to run the rollers for intake
+   */
+  public void rollersIntake(){
+    rollerMotor.set(1); // Set to desired speed for intake (Usually called from constants)
+  }
+  /**
+   * Method to run the rollers for outtake
+   */
+  public void rollersOuttake(){
+    rollerMotor.set(-1); // Set to desired speed for outtake (Usually called from constants)
+  }
+  /**
+   * Method to stop the rollers
+   */
+  public void rollersStop(){
+    rollerMotor.set(0); // Stop the rollers
+  }
+
+
+
+  /**
+   * Method to get the current velocity of the rollers
+   * @return double velocity in RPS
+   */
+  @AutoLogOutput
+  public double getRollersVelocity(){
+    return rollerMotor.getVelocity().getValueAsDouble();
+  }
+
+  /**
+   * Method to get the supply current of the rollers
+   * @return double supply current in Amps
+   */
+  @AutoLogOutput
+  public double getRollersSupplyCurrent(){
+    return rollerMotor.getSupplyCurrent().getValueAsDouble();
+  }
+
+  /**
+   * Method to get the stator current of the rollers
+   * @return double stator current in Amps
+   */
+  @AutoLogOutput
+  public double getRollersStatorCurrent(){
+    return rollerMotor.getStatorCurrent().getValueAsDouble();
+  }
+
+  /**
+   * Method to get the temperature of the rollers motor
+   * @return double temperature in Celsius
+   */
+  @AutoLogOutput
+  public double getRollersTemperature(){
+    return rollerMotor.getDeviceTemp().getValueAsDouble();
+  }
+
+  /**
+   * Method to get the voltage being applied to the rollers motor
+   * @return double voltage in Volts
+   */
+  @AutoLogOutput
+  public double getIntakeVoltage(){
+    return rollerMotor.getMotorVoltage().getValueAsDouble();
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
